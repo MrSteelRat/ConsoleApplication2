1a2f9381932c8461f29b043ef59cb91be8dbe971
MrRobot0ne <drebin341@gmail.com>
2021-08-24 17:25:44 +0300

    Упр 1.4

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-  std::cout << "Hello, world" << "\n";
+  std::cout << "Enter two numbers:" << "\n";
   int v1 = 0, v2 = 0;
   std::cin >> v1 >> v2;
   std::cout << "The sum of " << v1 << " and " << v2
-    << " is " << v1 + v2 << "\n";
+    << " is " << v1 * v2 << "\n";
 
-  return 0;
+
 }
