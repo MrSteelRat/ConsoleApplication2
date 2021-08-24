25dcc301c5e700d4fdc2e4f3184e01d3e6456d05
MrRobot0ne <drebin341@gmail.com>
2021-08-24 17:24:15 +0300

    Упраженние 1.3

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <istream>
 
 int main()
 {
-    return 0;
+  std::cout << "Hello, world" << "\n";
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "The sum of " << v1 << " and " << v2
+    << " is " << v1 + v2 << "\n";
+
+  return 0;
 }
