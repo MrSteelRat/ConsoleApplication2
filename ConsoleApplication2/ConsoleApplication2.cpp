ccab7169644077a35b83f29faa15578c02d3c992
MrRobot0ne <drebin341@gmail.com>
2021-08-24 17:51:54 +0300

    и пару <<

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,7 +6,7 @@ int main()
   std::cout << "Enter two numbers:" << "\n";
   int v1 = 0, v2 = 0;
   std::cin >> v1 >> v2;
-  std::cout "The sum of " << v1  << " and " << v2
+  std::cout << "The sum of " << v1  << " and " << v2
   << " is " << v1 + v2 << std::endl;
   return 0;
 
