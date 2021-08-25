e32aa47594a9865a74eb47b6b44fae2279ae39d5
MrRobot0ne <drebin341@gmail.com>
2021-08-25 18:48:03 +0300

    Упр  2.3 Вывод: 32 4294967264 32 -32 0 0

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
+﻿#include <iostream>
 #include <istream>
+#include "Sales_item.h"
 
 int main()
 {
-  int sum = 0, value = 0;
-  while (std::cin >> value)
-    sum += value;
-  std::cin >> sum >> value;
-  std::cout << " Sum is: " << sum << std::endl;
-  return 0;
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl;
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+		std::cout << i - i2 << std::endl;
+		std::cout << i - u << std::endl;
+		std::cout << u - i << std::endl;
+	return 0;
 }
